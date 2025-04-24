c73e1e29c3493d1e54895a7bb94f43fc1ed2d11e
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-04-24 12:57:28 +0700

    Add initial Flask application with basic routes for greeting and personal information

---
+++ Pertemuan3/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <h1>Hello, World!</h1>
+    <p>Selamat datang di aplikasi Flask Saya. </p>
+    '''
+
+@app.route('/datadiri')
+def data_diri():
+    return '''
+    <h1>Nama Saya, Muhamad Ibnu Khaidar Hafiz</h1>
+    <h2>NPM Saya 50421867</h2>
+    <p>Saya kelas 4IA15</p>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>NPM</th>
+            <th>Kelas</th>
+        </tr>
+        <tr>
+            <td>Muhamad Ibnu Khaidar Hafiz</td>
+            <td>50421867</td>
+            <td>4IA15</td>
+        </tr>
+    </table>
+    '''
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
