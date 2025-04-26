7d18869c7fa919cc09e81404922d3e6f2aa5626e
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-04-26 14:42:19 +0700

    Refactor data_diri route to improve greeting and presentation of personal information

--- Pertemuan3/app.py
+++ Pertemuan3/app.py
@@ -9,26 +9,48 @@ def hello_world():
     <p>Selamat datang di aplikasi Flask Saya. </p>
     '''
 
+# @app.route('/datadiri')
+# def data_diri():
+#     return '''
+#     <h1>Nama Saya, Muhamad Ibnu Khaidar Hafiz</h1>
+#     <h2>NPM Saya 50421867</h2>
+#     <p>Saya kelas 4IA15</p>
+#     <table border="1">
+#         <tr>
+#             <th>Nama</th>
+#             <th>NPM</th>
+#             <th>Kelas</th>
+#         </tr>
+#         <tr>
+#             <td>Muhamad Ibnu Khaidar Hafiz</td>
+#             <td>50421867</td>
+#             <td>4IA15</td>
+#         </tr>
+#     </table>
+#     '''
+
+## LA Pemrograman Jaringan
 @app.route('/datadiri')
 def data_diri():
     return '''
-    <h1>Nama Saya, Muhamad Ibnu Khaidar Hafiz</h1>
-    <h2>NPM Saya 50421867</h2>
-    <p>Saya kelas 4IA15</p>
+    <h1>Hello!</h1>
+    <p> salam kenal. ini data diri saya </p>
     <table border="1">
         <tr>
             <th>Nama</th>
-            <th>NPM</th>
-            <th>Kelas</th>
+            <td>Muhamad Ibnu Khaidar Hafiz</td>
         </tr>
         <tr>
-            <td>Muhamad Ibnu Khaidar Hafiz</td>
+            <th>NPM</th>
             <td>50421867</td>
+        </tr>
+        <tr>
+            <th>Kelas</th>
             <td>4IA15</td>
         </tr>
     </table>
-    '''
 
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
