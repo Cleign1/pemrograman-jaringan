fbaeeed855a0df1498c6eb2b28b32314d681237f
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-05-15 19:33:35 +0700

    Add video player functionality with play, pause, mute, and fullscreen controls

---
+++ Pertemuan6/static/js/video.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    var video = document.getElementById("videoPlayer");
+    var playBtn = document.getElementById("playBtn");
+    var pauseBtn = document.getElementById("pauseBtn");
+    var fullScreenBtn = document.getElementById("fullScreenBtn");
+    var muteBtn = document.getElementById("muteBtn");
+
+    video.muted = false;
+    video.volume = 0.5;
+    var lastVolume = video.volume;
+
+    playBtn.addEventListener("click", function () {
+        video.play();
+    });
+
+    pauseBtn.addEventListener("click", function () {
+        video.pause();
+    });
+
+    fullScreenBtn.addEventListener("click", function () {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+        }
+    });
+    muteBtn.addEventListener("click", function () {
+        if (video.muted) {
+            video.muted = false;
+            video.volume = lastVolume;
+            muteBtn.innerHTML = "Mute";
+        } else {
+            lastVolume = video.volume;
+            video.muted = true;
+            video.volume = 0;
+            muteBtn.innerHTML = "Unmute";
+        }
+    })
+});
